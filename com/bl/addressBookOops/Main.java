3f3c370af7ddfbf1ba1f059b43eab0a0ffc2047b
unknown <altamashbagwan1@gmail.com>
2021-10-07 17:57:38 +0530

    Added: Functioning of Welcome to Address Book

---
+++ com/bl/addressBookOops/Main.java
@@ -0,0 +1,10 @@
+package com.bl.addressBookOops;
+
+public class Main {
+
+	public static void main(String[] args) {
+			Message userInput = new Message();
+			userInput.message();
+	}
+
+}
---
+++ com/bl/addressBookOops/Message.java
@@ -0,0 +1,12 @@
+package com.bl.addressBookOops;
+
+public class Message {
+	
+		public void message() {
+			System.out.println("Welcome to Address Book Program");
+			
+		}
+
+}
+
+
