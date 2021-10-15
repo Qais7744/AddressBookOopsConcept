16f39c24779a69f2f1db30f812da1c9914725306
unknown <altamashbagwan1@gmail.com>
2021-10-15 23:31:08 +0530

    Added: Functioning of use Dictionary to create multiple AddressBook unique name

--- com/bl/addressBookOops/AddDetails.java
+++ com/bl/addressBookOops/AddDetails.java
@@ -5,22 +5,16 @@ import java.util.Scanner;
 
 public class AddDetails {
 	Scanner scanDetails = new Scanner(System.in);
-	ArrayList<PersonDetail> contactDetails = new ArrayList<PersonDetail>();
 
 	// Edit method use to edit the data.
 	public void editContact(ArrayList<PersonDetail> contactDetails) {
 		String data = scanDetails.nextLine();
-		int index = contactDetails.indexOf(data);
-		System.out.println(contactDetails.size());
-
 		for (int i = 0; i < contactDetails.size(); i++) {
-
 			if (contactDetails.get(i).getFirstName().equals(data)) {
 				System.out.println("1. Enter The update First Name :");
 				System.out.println("2. Enter The update LastName. ");
 				System.out.println("3. Enter The update Address. ");
 				System.out.println("4. Enter The update City. ");
-				
 				System.out.println("5. Enter The update State. ");
 				System.out.println("6. Enter The update ZipCode. ");
 				System.out.println("7. Enter The update PhoneNumber. ");
@@ -35,41 +29,49 @@ public class AddDetails {
 					String firstNameData = scanDetails.nextLine();
 					contactDetails.get(i).setFirstName(firstNameData);
 					break;
+
 				case 2:
 					System.out.println("Enter the Last Name");
 					String lastNameData = scanDetails.nextLine();
 					contactDetails.get(i).setLastName(lastNameData);
 					break;
+
 				case 3:
 					System.out.println("Enter the Address");
 					String addressData = scanDetails.nextLine();
 					contactDetails.get(i).setAddress(addressData);
 					break;
+
 				case 4:
 					System.out.println("Enter the City");
 					String cityData = scanDetails.nextLine();
 					contactDetails.get(i).setCity(cityData);
 					break;
+
 				case 5:
 					System.out.println("Enter the State");
 					String stateData = scanDetails.nextLine();
 					contactDetails.get(i).setState(stateData);
 					break;
+
 				case 6:
 					System.out.println("Enter the Zip Code");
 					String zipCodeData = scanDetails.nextLine();
 					contactDetails.get(i).setZipCode(zipCodeData);
 					break;
+
 				case 7:
 					System.out.println("Enter the Phone Number");
 					String phoneNumberData = scanDetails.nextLine();
 					contactDetails.get(i).setPhoneNumber(phoneNumberData);
 					break;
+
 				case 8:
 					System.out.println("Enter the Email");
 					String emailData = scanDetails.nextLine();
 					contactDetails.get(i).setEmail(emailData);
 					break;
+
 				default:
 					System.out.println("you have not update any details");
 					break;
@@ -77,16 +79,17 @@ public class AddDetails {
 			}
 		}
 	}
+
 	// Delete method use to delete the data.
-		public void deleteContact(ArrayList<PersonDetail> contatctDetails) {
-			System.out.println("Enter The Name");
-			String data = scanDetails.nextLine();
-			for (int i = 0; i < contatctDetails.size(); i++) {
-				if (contatctDetails.get(i).getFirstName().equals(data)) {
-					contatctDetails.remove(i);
-				
-				} else
-					System.out.println("not match any details");
-			}
+	public void deleteContact(ArrayList<PersonDetail> contatctDetails) {
+		System.out.println("Enter The Name");
+		String data = scanDetails.nextLine();
+		for (int i = 0; i < contatctDetails.size(); i++) {
+			if (contatctDetails.get(i).getFirstName().equals(data)) {
+				contatctDetails.remove(i);
+
+			} else
+				System.out.println("not match any details");
 		}
+	}
 }
--- com/bl/addressBookOops/AddressBook.java
+++ com/bl/addressBookOops/AddressBook.java
@@ -1,6 +1,8 @@
 package com.bl.addressBookOops;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
 // Java program to read data of various types using a scanner class.
 import java.util.Scanner;
 
@@ -9,6 +11,8 @@ public class AddressBook {
 	static ArrayList<PersonDetail> contact = new ArrayList<PersonDetail>();
 	public static Scanner scanner = new Scanner(System.in);
 	static int menu;
+	static Dictionary dictionary = new Hashtable();
+	static Hashtable<Integer, ArrayList<PersonDetail>> hashTable = new Hashtable<Integer, ArrayList<PersonDetail>>();
 
 	public static int choice() {
 		// Print the value to choice.
@@ -19,66 +23,78 @@ public class AddressBook {
 		menu = scanner.nextInt();
 		return menu;
 	}
-	
+
+	private static int inputInteger() {
+		int integerInput = scanner.nextInt();
+		return integerInput;
+	}
+
 	public static void main(String[] args) {
-		// we create an object from the AddDetails class named addPersonDetail.
+		System.out.println("Enter The Number of add Multiple AddressBook");
+		int numberOfAddressBook = inputInteger();
 		AddDetails addPersonDetail = new AddDetails();
-		
-		// we use while loop
-		menu = choice();
-		while (menu != 0) {
-			if (menu > 1)
-				menu = choice();
-			//use to switch case to easily add details and edit details.			
-			switch (menu) {
-			case 1:
-				while (menu != 2) {
+		for (int i = 1; i < numberOfAddressBook; i++) {
+			// we use while loop
+			menu = choice();
+			while (menu != 0) {
+				if (menu > 1)
+					menu = choice();
+				// use to switch case to easily add details and edit details.
+				switch (menu) {
+				case 1:
+					while (menu != 2) {
 
-					System.out.println("Enter First Name: ");
-					String firstName = scanner.next();
-					System.out.println("Enter Last Name: ");
-					String lastName = scanner.next();
-					System.out.println("Enter Address: ");
-					String address = scanner.next();
-					System.out.println("Enter City: ");
-					String city = scanner.next();
-					System.out.println("Enter State: ");
-					String state = scanner.next();
-					System.out.println("Enter a zipCode: ");
-					String zipCode = scanner.next();
-					System.out.println("Enter PhoneNumber: ");
-					String phoneNumber = scanner.next();
-					System.out.println("Enter email: ");
-					String email = scanner.next();
-					PersonDetail personDetail = new PersonDetail(firstName, lastName, address, city, state, zipCode,
-							phoneNumber, email);
-					contact.add(personDetail);
-					System.out.println("Would you like to add someone else? 1: Yes, 2: No");
-					menu = scanner.nextInt();
+						System.out.println("Enter First Name: ");
+						String firstName = scanner.next();
+						System.out.println("Enter Last Name: ");
+						String lastName = scanner.next();
+						System.out.println("Enter Address: ");
+						String address = scanner.next();
+						System.out.println("Enter City: ");
+						String city = scanner.next();
+						System.out.println("Enter State: ");
+						String state = scanner.next();
+						System.out.println("Enter a zipCode: ");
+						String zipCode = scanner.next();
+						System.out.println("Enter PhoneNumber: ");
+						String phoneNumber = scanner.next();
+						System.out.println("Enter email: ");
+						String email = scanner.next();
+						PersonDetail personDetail = new PersonDetail(firstName, lastName, address, city, state, zipCode,
+								phoneNumber, email);
+						contact.add(personDetail);
+						System.out.println("Would you like to add someone else? 1: Yes, 2: No");
+						menu = scanner.nextInt();
+					}
+					break;
+
+				case 2:
+					System.out.println("Enter First Name of contact that you would like to edit: ");
+					addPersonDetail.editContact(contact);
+					break;
+
+				case 3:
+					addPersonDetail.deleteContact(contact);
+					break;
+
+				default:
+					System.out.println("Complete Process update Contact Details");
+					break;
+
 				}
-				break;
-
-			case 2:
-				System.out.println("Enter First Name of contact that you would like to edit: ");
-				addPersonDetail.editContact(contact);
-				break;
-				
-			case 3:
-				addPersonDetail.deleteContact(contact);
-				break;
-
-			default:
-				System.out.println("Complete Process update Contact Details");
-				break;
-
 			}
-			//for loop use to print the data after add aur edit.
-			for (int i = 0; i < contact.size(); i++)
-				System.out.println(contact.get(i));
-			{
-				System.out.println("Goodbye!");
-				System.out.println();
-			}
+			hashTable.put(i, contact);
+		}
+		// for loop use to print the data after add or edit data.
+		for (int i = 0; i < contact.size(); i++)
+			System.out.println(contact.get(i));
+		for (int i = 1; i <= hashTable.size(); i++) {
+			System.out.println("address book " + i);
+			System.out.println(hashTable.get(i));
+		}
+		{
+			System.out.println("Goodbye!");
+			System.out.println();
 		}
 	}
 }
