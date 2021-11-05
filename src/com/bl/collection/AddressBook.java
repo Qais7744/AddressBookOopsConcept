59a9b8ee39feec633ee3af4a64f89106033fc089
unknown <altamashbagwan1@gmail.com>
2021-11-06 00:00:44 +0530

    Added: Functioning of AddressBook Collection use

---
+++ src/com/bl/collection/AddressBook.java
@@ -0,0 +1,98 @@
+package com.bl.collection;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class AddressBook {
+    public static Scanner scanner = new Scanner(System.in);
+    static Hashtable<Integer, ArrayList<PersonDetail>> hashTable = new Hashtable<>();
+    static int choice;
+
+    public static int choice() {
+        // Print the value to choice.
+        System.out.println(" 0. Exit. ");
+        System.out.println(" 1. Add contact. ");
+        System.out.println(" 2. Edit contact. ");
+        System.out.println(" 3. Delete contact. ");
+        choice = scanner.nextInt();
+        return choice;
+    }
+
+    private static int inputInteger() {
+        int integerInput = scanner.nextInt();
+        return integerInput;
+    }
+
+    public static void main(String[] args) {
+        // Use HashTable to add Multiple AddressBook in Dictionary.
+        System.out.println("Enter The Number of add Multiple AddressBook");
+        int numberOfAddressBook = inputInteger();
+        for (int i = 1; i <= numberOfAddressBook; i++) {
+            System.out.println("AddressBook " + i);
+            ArrayList<PersonDetail> contact = new ArrayList<>();
+            // We use while loop
+            choice = choice();
+            while (choice != 0) {
+                AddDetails addPersonDetail = new AddDetails();
+                System.out.println("Would you like to add someone else? 1: Yes, 2: No");
+                choice = scanner.nextInt();
+                if (choice > 1)
+                    choice = choice();
+                // Used to switch case to easily add details, edit details and delete details.
+                switch (choice) {
+
+                    case 1:
+                        System.out.println("Enter First Name: ");
+                        String firstName = scanner.next();
+                        System.out.println("Enter Last Name: ");
+                        String lastName = scanner.next();
+                        System.out.println("Enter Address: ");
+                        String address = scanner.next();
+                        System.out.println("Enter City: ");
+                        String city = scanner.next();
+                        System.out.println("Enter State: ");
+                        String state = scanner.next();
+                        System.out.println("Enter a zipCode: ");
+                        String zipCode = scanner.next();
+                        System.out.println("Enter PhoneNumber: ");
+                        String phoneNumber = scanner.next();
+                        System.out.println("Enter email: ");
+                        String email = scanner.next();
+                        PersonDetail personDetail = new PersonDetail(firstName, lastName, address, city, state, zipCode,
+                                phoneNumber, email);
+                        boolean addNewEntry = addPersonDetail.duplicateDetailsRemove(contact);
+                        System.out.println("The value is" + " " + addNewEntry);
+                        if (addNewEntry)
+                            contact.add(personDetail);
+                        else
+                            System.out.println("Finally Duplicate Entry Remove");
+                        break;
+
+                    case 2:
+                        System.out.println("Enter First Name of contact that you would like to edit: ");
+                        addPersonDetail.editContact(contact);
+                        break;
+
+                    case 3:
+                        addPersonDetail.deleteContact(contact);
+                        break;
+
+                    default:
+                        System.out.println("Complete Process update Contact Details");
+                        break;
+
+                }
+            }
+            hashTable.put(i, contact);
+        }
+        for (int i = 1; i <= hashTable.size(); i++) {
+            System.out.println("address book " + i);
+            System.out.println(hashTable.get(i));
+        }
+        {
+            System.out.println("Goodbye!");
+            System.out.println();
+        }
+    }
+}
---
+++ src/com/bl/collection/PersonDetail.java
@@ -0,0 +1,66 @@
+package com.bl.collection;
+
+public class PersonDetail {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String phoneNumber;
+    private String email;
+
+    public PersonDetail(String firstName, String lastName, String address, String city, String state, String zipCode, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Data [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+                + ", state=" + state + ", zipCode=" + zipCode + ", phoneNumber=" + phoneNumber + ", email=" + email
+                + "]";
+    }
+}
