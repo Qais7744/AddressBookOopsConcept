f5491746b34479e7e0c24a9188fde31d5bb563f6
unknown <altamashbagwan1@gmail.com>
2021-11-09 01:21:22 +0530

    Added: Refactored code some changes in SearchDetails method

--- src/com/bl/collection/AddDetails.java
+++ src/com/bl/collection/AddDetails.java
@@ -109,7 +109,7 @@ public class AddDetails {
         int totalNumberOfCount = 0;
         for (int i = 1; i <= hashtable.size(); i++) {
             List<PersonDetail> numberOfCity = hashtable.get(i).stream().filter(search -> search.getCity().equalsIgnoreCase(cityOrStateName)
-                    || search.getState().equals(cityOrStateName)).collect(Collectors.toList());
+                                              || search.getState().equals(cityOrStateName)).collect(Collectors.toList());
             System.out.println("Details of CityOrState " + numberOfCity + "\n"
                     + "Number of Times " + numberOfCity.size());
         }
