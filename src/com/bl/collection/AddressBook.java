e5195bec921ffbdf7da93b62d9b1708cb768a82e
unknown <altamashbagwan1@gmail.com>
2021-11-13 01:34:25 +0530

    Added: CSVWriter class code

--- src/com/bl/collection/AddressBook.java
+++ src/com/bl/collection/AddressBook.java
@@ -89,24 +89,28 @@ public class AddressBook {
             }
             hashTable.put(i, contact);
             addPersonDetail.writeToFile(hashTable);
+            addPersonDetail.writeToFileInOpenCsv(hashTable);
         }
         for (int i = 1; i <= hashTable.size(); i++) {
             System.out.println("address book " + i);
             System.out.println(hashTable.get(i));
         }
         System.out.println();
-        System.out.println("Reading AddressBook From File");
+        System.out.println("Reading AddressBook from File");
         addPersonDetail.readFromFile();
+        System.out.println();
+        System.out.println("Reading AddressBook from File");
+        addPersonDetail.readFromFileInOpenCsv();
         System.out.println();
         addPersonDetail.searchDetails(hashTable);
         System.out.println();
-        System.out.println("After Sorting The FirstName ");
+        System.out.println("After sorting the FirstName ");
         addPersonDetail.sortFirstName(hashTable);
         System.out.println();
-        System.out.println("After Sorting The City ");
+        System.out.println("After sorting the City ");
         addPersonDetail.sortCity(hashTable);
         System.out.println();
-        System.out.println("After Sorting The ZipCode ");
+        System.out.println("After sorting the ZipCode ");
         addPersonDetail.sortZipCode(hashTable);
         {
             System.out.println("Goodbye!");
